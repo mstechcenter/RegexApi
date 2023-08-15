f794e3e0045ede58c0789ee239aada9ac775d05d
koutaro harada <haradaktr@intellilink.co.jp>
2023-08-15 14:02:40 +0900

    First commit.

---
+++ RegexAPI/__init__.py
@@ -0,0 +1,28 @@
+import logging
+import json
+import re
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    req_body = req.get_json()
+
+    content = req_body.get('content')
+    pattern = req_body.get('pattern')
+
+    repatter = re.compile(pattern)
+    result = repatter.search(content)
+
+    return func.HttpResponse(
+        json.dumps({
+            'method': req.method,
+            'url': req.url,
+            'headers': dict(req.headers),
+            'params': dict(req.params),
+            'content': content,
+            'pattern': pattern,
+            'result': result.group()
+        }),
+        mimetype="application/json"
+    )
