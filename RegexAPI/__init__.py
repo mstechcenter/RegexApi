3a5a6b220ae33e61e4658b58d53d57de6ea012d2
koutaro harada <haradaktr@intellilink.co.jp>
2023-08-15 14:48:27 +0900

    Edit __init__.py.

--- RegexAPI/__init__.py
+++ RegexAPI/__init__.py
@@ -4,7 +4,7 @@ import re
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('RegexAPI processed a request.')
 
     req_body = req.get_json()
 
